eb0e203214a4e1f3bfa22190821f8d48d6ec8c39
Andrej Burja <andrej.burja@gmail.com>
2025-03-28 12:37:07 +0100

    way type amount can be 1

--- kompy/way_type.py
+++ kompy/way_type.py
@@ -10,11 +10,11 @@ class WayType:
         """
         Initialize the way type information.
         :param way_type: Type of the way.
-        :param amount: Amount, must be greater than 0 and less than 1.
+        :param amount: Amount, must be greater than 0 and less than or equal 1.
         """
         if way_type not in PossibleWayType.list_all():
             raise ValueError(f'Invalid way type provided: {way_type}. Please provide a valid way type.')
-        if not (0 < amount < 1):
-            raise ValueError('Amount must be greater than 0 and less than 1.')
+        if not (0 < amount <= 1):
+            raise ValueError('Amount must be greater than 0 and less than or equal 1.')
         self.type = way_type
         self.amount = amount
